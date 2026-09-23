98d54559f7153f016cb10d3215656faf69b2e15f
FazendinhaTurca <fazendinhaturca@gmail.com>
2026-09-23 20:33:55 -0300

    Add files via upload
    atualizado

--- scripts/generate-product-pages.js
+++ scripts/generate-product-pages.js
@@ -951,7 +951,7 @@ async function main() {
 
   const xml = [
     '<?xml version="1.0" encoding="UTF-8"?>',
-    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/image-sitemap/1.1">'
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">'
   ];
 
   for (const [url, meta] of sitemap) {
